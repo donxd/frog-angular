53e8b3d6261a250839e0fe228d44b7bc89848078
donxd <n0s3.xd@gmail.com>
2019-11-07 18:24:31 -0600

    feat(frog): add frog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrogComponent } from './frog/frog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/frog/frog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frog',
+  templateUrl: './frog.component.html',
+  styleUrls: ['./frog.component.sass']
+})
+export class FrogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
