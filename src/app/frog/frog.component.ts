0def7a78d0275accab830084bb8675b089fdf877
donxd <n0s3.xd@gmail.com>
2019-11-08 01:17:14 -0600

    feat(frog): add frog style and jump

--- src/app/frog/frog.component.ts
+++ src/app/frog/frog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-frog',
@@ -7,9 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrogComponent implements OnInit {
 
-  constructor() { }
+  @Input() boxes: number;
+  @Input() selected: number;
+  @Input() config: boolean;
+
+  readonly DEFAULT_BOXES = 5;
+  readonly DEFAULT_SELECTED = 1;
+  readonly DEFAULT_CONFIG = false;
+  readonly CSS_STYLE_FROG = 'frog';
+
+  private boxesComponent: number;
+  private selectedComponent: number;
+  private configComponent: boolean;
+
+  private boxesElements;
+
+  constructor() {
+    // console.log('constructor - boxes : ', this.boxes);
+    // console.log('constructor - selected : ', this.selected);
+    // console.log('constructor - config : ', this.config);
+  }
 
   ngOnInit() {
+    this.boxesComponent = this.boxes !== undefined ?
+      this.boxes : this.DEFAULT_BOXES;
+
+    this.selectedComponent = this.selected !== undefined ?
+      Number.parseInt(`${this.selected}`) : this.DEFAULT_SELECTED;
+
+    this.configComponent = this.config !== undefined ?
+      this.config : this.DEFAULT_CONFIG;
+
+    this.generateObjectBoxes();
+  }
+
+  generateObjectBoxes () {
+    this.boxesElements = [];
+    for (let i = 0; i < this.boxesComponent; i++) {
+      const box = this.getNewBoxObject(i+1, this.selectedComponent);
+
+      this.boxesElements.push(box);
+    }
+  }
+
+  getNewBoxObject (index, itemSelected) {
+    const attrSelected = index === itemSelected;
+
+    return this.getBoxObject(index, attrSelected);
+  }
+
+  getBoxObject (index, attrSelected) {
+    return {
+      selected: attrSelected, 
+      index
+    };
+  }
+
+  getIndex (item) {
+    return item.index;
+  }
+
+  getBoxClass (item) {
+    return item.selected ? this.CSS_STYLE_FROG : '';
+  }
+
+  jumpFrogAdv (boxSelected) {
+    if (boxSelected.selected) {
+      const newBoxSelection = boxSelected.index < this.boxesElements.length ?
+        this.boxesElements[ boxSelected.index ] : this.boxesElements[ 0 ];
+
+      this.boxesElements[boxSelected.index-1].selected = false;
+      this.boxesElements[newBoxSelection.index-1].selected = true;
+    }
   }
 
 }
